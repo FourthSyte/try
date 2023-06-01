501875dc01fa63d90cb2c38a0abfc78c50999b66
FourthSyte <giddyroble@gmail.com>
2023-06-02 07:30:22 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import streamlit as st
+from streamlit_webrtc import webrtc_streamer, WebRtcMode
+import av
+
+from sample_utils.turn import get_ice_servers
+
+st.title('Example')
+
+
+def video_frame_callback(frame):
+    img = frame.to_ndarray(format="bgr24")
+
+    ##flipped = img[::-1,:,:]
+
+    return av.VideoFrame.from_ndarray(format="bgr24")
+
+
+webrtc_streamer(
+    key="example",
+    mode=WebRtcMode.SENDRECV,
+    rtc_configuration={"iceServers": get_ice_servers()},
+    video_frame_callback=video_frame_callback,
+    media_stream_constraints={"video": True, "audio": False},
+    async_processing=True,
+
+)
